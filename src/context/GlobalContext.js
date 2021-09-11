782bea506a3106410addbc766c31a1b569ab699f
fahadaleem <faleem396@gmail.com>
2021-09-11 12:12:54 -0700

    Login function completed.

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -7,7 +7,10 @@ import {
   getDatabase,
   set,
   ref,
+  child,
+  get,
 } from "../firebase/firebaseConfig";
+import Swal from "sweetalert2";
 
 const GlobalContext = createContext();
 
@@ -17,21 +20,52 @@ const GlobalProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const history = useHistory();
 
+  // login user function
   const handleLoginUser = (userData) => {
-    const auth = getAuth();
-    console.log("userData", userData);
-    signInWithEmailAndPassword(auth, userData.email, userData.password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        setUser(user);
-        // ...
+
+    // referece of database
+    const dbRef = ref(getDatabase());
+    // generate key from the email and account type
+    const userId = handleGenerateUniqueKey(
+      userData.accountType,
+      userData.email
+    );
+    // search the key in the database
+    get(child(dbRef, `users/${userId}`))
+      .then((snapshot) => {
+          // if the key is available in the database then login the user
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          const auth = getAuth();
+          // login function
+          console.log("userData", userData);
+          signInWithEmailAndPassword(auth, userData.email, userData.password)
+            .then((userCredential) => {
+              // Signed in
+              const user = userCredential.user;
+              setUser(user);
+              Swal.fire({
+                icon: "success",
+                title: "Login Successful",
+              });
+              // ...
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              setError(errorMessage);
+              // ..
+            });
+        } else {
+            // if no key is available then show message
+          Swal.fire({
+            icon: "error",
+            title: "No data available",
+          });
+        }
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError(errorMessage);
-        // ..
+        console.error(error);
       });
   };
 
@@ -41,14 +75,18 @@ const GlobalProvider = ({ children }) => {
     return accountType + "-" + emailKey;
   };
 
+  // function to signup a user
   const handleSignupUser = (userDetails) => {
     const auth = getAuth();
+    // signup a user with email and password
     createUserWithEmailAndPassword(
       auth,
       userDetails.email,
       userDetails.password
     )
+      // if signup is successful
       .then((userCredential) => {
+        // generate unique key and save it to database
         const key = handleGenerateUniqueKey(
           userDetails.accountType,
           userDetails.email
@@ -60,9 +98,14 @@ const GlobalProvider = ({ children }) => {
         });
         // Signed in
         const user = userCredential.user;
-        history.push("/login");
+        Swal.fire({
+          icon: "success",
+          title: "Account Created",
+          text: "You can now login",
+        }).then((resp) => history.push("/login"));
         // ...
       })
+      // if there is an error show error message
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { getDatabase, ref, set } from "firebase/database";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,16 +20,18 @@ const firebaseConfig = {
   storageBucket: "campus-recruitmentapp.appspot.com",
   messagingSenderId: "840049800266",
   appId: "1:840049800266:web:86df9a38e3b396dbd4a2b3",
-  measurementId: "G-QJLDM487S6"
+  measurementId: "G-QJLDM487S6",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    getDatabase,
-    ref,
-    set,
-}
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  getDatabase,
+  ref,
+  set,
+  child,
+  get,
+};
